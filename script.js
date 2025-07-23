8acd62f72238728c7be9e50f3a693955dda8d5b7
ValeriaTorrecilla <torrecillov@gmail.com>
2025-07-22 18:21:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -31,8 +31,36 @@ const data = {
         { nombre: "Materiales I", creditos: 10, requisitos: ["Química"] },
         { nombre: "Termodinámica", creditos: 10, requisitos: [] },
         { nombre: "Optativas de ciencias sociales y humanidades", creditos: 6, requisitos: [] }
+    ],
+    "Quinto semestre": [
+        { nombre: "Análisis de circuitos", creditos: 10, requisitos: [] },
+        { nombre: "Elementos de mecánica del medio continuo", creditos: 8, requisitos: ["Cálculo vectorial"] },
+        { nombre: "Análisis numérico", creditos: 8, requisitos: ["Ecuaciones diferenciales"] },
+        { nombre: "Materiales II", creditos: 10, requisitos: ["Materiales I"] },
+        { nombre: "Termodinámica aplicada", creditos: 8, requisitos: ["Termodinámica"] },
+        { nombre: "Sistemas de conversión de energía térmica", creditos: 2, requisitos: ["Termodinámica"] }
+    ],
+    "Sexto semestre": [
+        { nombre: "Mecánica de sólidos", creditos: 8, requisitos: [] },
+        { nombre: "Modelado de sistemas físicos", creditos: 8, requisitos: [] },
+        { nombre: "Manufactura II", creditos: 10, requisitos: ["Manufactura I"] },
+        { nombre: "Mecánica de fluidos I", creditos: 10, requisitos: ["Termodinámica aplicada" & "Sistemas de conversión de energía térmica"] },
+        { nombre: "Introducción a la economía", creditos: 8, requisitos: [] }
+    ],
+    "Séptimo semestre": [
+        { nombre: "Electrónica básica", creditos: 10, requisitos: ["Análisis de circuitos"] },
+        { nombre: "Mecanismos", creditos: 8, requisitos: [] },
+        { nombre: "Ética profesional", creditos: 6, requisitos: [] },
+        { nombre: "Ingeniería económica", creditos: 8, requisitos: [] },
+        { nombre: "Mecánica de fluidos II", creditos: 10, requisitos: ["Elementos de mecánica del medio continuo"] }
+    ],
+    "Octavo semestre": [
+        { nombre: "Diseño de elementos de máquinas", creditos: 8, requisitos: ["Mecánica de sólidos"] },
+        { nombre: "Dinámica de maquinaria", creditos: 10, requisitos: ["Mecanismos"] },
+        { nombre: "Instrumentación y control", creditos: 8, requisitos: [] },
+        { nombre: "Transferencia de calor", creditos: 10, requisitos: [] },
+        { nombre: "Recursos y necesidades de México", creditos: 8, requisitos: [] },
     ]
-    // Agrega más semestres aquí si quieres
 };
 
 function crearMalla() {
